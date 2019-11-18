f625a9c6df97c4d58d99e8aa6c89ec9349ebba3c
ChihCheng Lai <omygoddas@gmail.com>
2019-11-18 22:12:41 +0800

    Updates

---
+++ precache-manifest.9c229ec63e10fcd33ca0aa5c7bd0f0b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e860c3c3d5eda86f6d690cab8f4068",
+    "url": "/ztm-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba7becb229f6cf6c658d",
+    "url": "/ztm-monsters-rolodex/static/css/main.7c2623f1.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/ztm-monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "ba7becb229f6cf6c658d",
+    "url": "/ztm-monsters-rolodex/static/js/main.10354ad2.chunk.js"
+  },
+  {
+    "revision": "a085fb6af09dadfbc7b7",
+    "url": "/ztm-monsters-rolodex/static/js/runtime-main.f49ca0b8.js"
+  }
+]);
